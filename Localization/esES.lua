967c5ec26d0ff271fd23f59986281ad93a9e5c6f
Jai <814683@qq.com>
2020-03-04 21:23:29 +0800

    Added locale files

---
+++ Localization/esES.lua
@@ -0,0 +1,6 @@
+
+local L = LibStub('AceLocale-3.0'):NewLocale('KiwiFarm', 'esES', false, true)
+if not L then return end
+
+--@localization(locale=""esES", format="lua_additive_table", table-name="L", same-key-is-true=true)@
+
